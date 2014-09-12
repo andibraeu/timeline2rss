5570efd7980991dd7074e750d1ceaa5b5eeec8e0
andibraeu <ab@andi95.de>
2014-09-12 20:09:32 +0200

    create rss feed from facebook timeline

--- tl2rss.php
+++ tl2rss.php
@@ -10,14 +10,21 @@ use Facebook\FacebookSession;
 use Facebook\GraphObject;
 use Facebook\FacebookRequest;
 use Facebook\FacebookRequestException;
-use Argentum\FeedBundle\Feed\Feed;
-
+use Suin\RSSWriter\Feed;
+use Suin\RSSWriter\Channel;
+use Suin\RSSWriter\Item;
 
 require 'vendor/autoload.php';
 
 $app_id = "";
 $app_secret = "";
 $page_id = '';
+
+$channelTitle = "Weimarnetz Facebook News";
+$channelDesc = "Wir veröffentlichen hier unsere Neuigkeiten aus unserer Facebook-Timeline";
+$channelUrl = "http://www.weimarnetz.de";
+$channelLanguage = "de-DE";
+$channelCopyRight = "2014, Weimarnetz";
 
 FacebookSession::setDefaultApplication($app_id, $app_secret);
 
@@ -29,18 +36,35 @@ $session = new FacebookSession($app_id."|".$app_secret);
  * @param int $titleLength
  * @return mixed
  */
-function splitMessage($message, $returnItem, $titleLength=100) {
+function splitMessage($message, $returnItem, $titleLength=80) {
     $messageArray = explode("\n", $message, 2);
     if (count($messageArray) > 1 ) {
         $title = $messageArray[0];
         $content = $messageArray[1];
     } else {
-
+        $title = splitMessage(wordwrap($message, $titleLength), "title");
+        $content = str_replace($title, "...", $message);
+        $title .= "...";
     }
-    return $message;
+    if ($returnItem == "title") {
+        return $title;
+    } else {
+        return $content;
+    }
 }
 
-//$session = FacebookSession::newAppSession();
+$feed = new Feed();
+
+$channel = new Channel();
+$channel
+    ->title($channelTitle)
+    ->description($channelDesc)
+    ->url($channelUrl)
+    ->language($channelLanguage)
+    ->copyright($channelCopyRight)
+    ->pubDate(date("U"))
+    ->lastBuildDate(date("U"))
+    ->appendTo($feed);
 
 // Make a new request and execute it.
 try {
@@ -52,18 +76,31 @@ try {
         if (empty($message)) {
             continue;
         }
+        $id = $dings->getProperty('id');
         $title = splitMessage($message, "title");
         $content = splitMessage($message, "content");
-        $createdAt = $dings->getProperty('created_time');
-        $updatedAt = $dings->getProperty('updated_time');
+        $createdAt = date("U",strtotime($dings->getProperty('created_time')));
+        $updatedAt = strtotime($dings->getProperty('updated_time'));
         $link = $dings->getProperty('link');
         $picture = $dings->getProperty('picture');
-        print("$type: " . $message . "<br/>");
+        //print("$title: <br/>" . $content . "<br/><br/>");
+        $item = new Item();
+        $item
+            ->title($title)
+            ->url("https://facebook.com/$id")
+            ->pubDate($updatedAt)
+            ->guid("https://facebook.com/$id", true)
+            ->description($content)
+            ->appendTo($channel);
     }
 } catch (FacebookRequestException $ex) {
     echo $ex->getMessage();
 } catch (\Exception $ex) {
     echo $ex->getMessage();
 }
+
+// set the header type
+header("Content-type: text/xml");
+echo $feed->render();
 
 ?>
